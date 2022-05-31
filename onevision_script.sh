256e1f03691a9a23733d7d787dcfa34c22e1685c
CarlosFelixxs <carlos.fsilva@bandtec.com.br>
2022-05-31 10:22:28 -0300

    reta final

--- onevision_script.sh
+++ onevision_script.sh
@@ -14,20 +14,18 @@ fi
 echo "reiniciando o terminal"
 source "/home/$usuario/.sdkman/bin/sdkman-init.sh"
 
+echo "atualizando os pacotes"
+sudo apt update && sudo apt upgrade.
 
 echo "Checando se Java já esta instalado"
 which java
 if [ $? = 0 ]
 then echo "Já possui java instalado"
 else echo "Instalando java"
-sdk install java 11.0.12.7.1-amzn
+sudo apt install default-jre ; apt install openjdk-11-jre-headless; -y
 fi
 echo "versão do instalada: "
 java -version
-
-echo "atualizando os pacotes"
-sudo apt update && sudo apt upgrade.
-
 
 echo "procurando o docker"
 which docker
@@ -44,9 +42,8 @@ sudo systemctl enable docker
 echo "baixando mysql"
 sudo docker pull mysql:8.0.16
 echo "criando o banco de dados"
+sudo docker run -d -p 3306:3306 --name onevisionBD -e "MYSQL_DATABASE =onevision" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:8.0.16
 sudo docker build -t onevision_img:1.0 .
-echo "executando mysql"
-sudo docker run -d -p 3306:3306 --name onevision onevision_img:1.0
 
 echo "Clonando o repositorio da aplicação OneVision."
 https://github.com/CarlosFelixxs/onevision-jar.git
